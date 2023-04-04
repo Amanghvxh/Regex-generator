b170549a25261a34a4567a56534a706095ff5368
Amandeep Jandu <amandeepjandu@192-168-1-106.tpgi.com.au>
2023-04-05 00:46:30 +1000

    fixing up server

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const xss = require("xss-clean");
 const helmet = require("helmet");
 const rateLimiter = require("express-rate-limit");
 const CheckPrevMatch = require("./middlewares/checkPrev");
+const notFound = require("./middlewares/not-found");
 
 app.use(xss());
 app.use(helmet());
@@ -24,6 +25,10 @@ app.use(
 );
 app.use(express.json());
 app.use(CheckPrevMatch);
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: "This is root of the server" });
+});
 app.use("/api/v1/build", regexRouter);
+app.use(notFound);
 
 app.listen(500, console.log("server is running...."));
